97f58a63eafd4f53740d710d5c4a6109b5b157dc
heavyrainik <sobolev.as@phystech.edu>
2021-12-13 23:25:03 +0300

    code fixed

--- src/main/java/ru/sbt/mipt/oop/HomeBuilder.java
+++ src/main/java/ru/sbt/mipt/oop/HomeBuilder.java
@@ -31,28 +31,28 @@ public class HomeBuilder {
 
     private static Hall createTestHall() {
         Collection<Light> testLights = Arrays.asList(new Light("7", false), new Light("8", false), new Light("9", false));
-        //Collection<Door> testDoors = Arrays.asList(new Door("4", false));
+        Collection<Door> testDoors = Arrays.asList(new Door("4", false));
 
         return new Hall(testLights, testDoors, "hall");
     }
 
     private static Room createTestBedroom() {
         Collection<Light> testLights = Arrays.asList(new Light("4", false), new Light("5", false), new Light("6", false));
-        //Collection<Door> testDoors = Arrays.asList(new Door("3", false));
+        Collection<Door> testDoors = Arrays.asList(new Door("3", false));
 
         return new Room(testLights, testDoors, "bedroom");
     }
 
     private static Room createTestBathroom() {
         Collection<Light> testLights = Arrays.asList(new Light("3", false));
-        //Collection<Door> testDoors = Arrays.asList(new Door("2", false));
+        Collection<Door> testDoors = Arrays.asList(new Door("2", false));
 
         return new Room(testLights, testDoors, "bathroom");
     }
 
     private static Room createTestKitchen() {
         Collection<Light> testLights = Arrays.asList(new Light("1", false), new Light("2", false));
-        //Collection<Door> testDoors = Arrays.asList(new Door("1", false));
+        Collection<Door> testDoors = Arrays.asList(new Door("1", false));
 
         return new Room(testLights, testDoors, "kitchen");
     }
