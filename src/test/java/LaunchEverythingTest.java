6a8839451a1f141c42f48b7b2536beaf517a533f
heavyrainik <sobolev.as@phystech.edu>
2021-12-13 19:10:53 +0300

    test

---
+++ src/test/java/LaunchEverythingTest.java
@@ -0,0 +1,15 @@
+import com.coolcompany.smarthome.events.SensorEventsManager;
+import config.SmartHomeConfiguration;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+
+
+public class LaunchEverythingTest {
+	@Test
+	void testEverything() {
+		AbstractApplicationContext context = new AnnotationConfigApplicationContext(SmartHomeConfiguration.class);
+		SensorEventsManager sensorEventsManager = context.getBean(SensorEventsManager.class);
+		sensorEventsManager.start();
+    }
+}
