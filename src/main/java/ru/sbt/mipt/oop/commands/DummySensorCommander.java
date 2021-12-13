ff544337b452d940be3641f2f089775bbb16107f
heavyrainik <sobolev.as@phystech.edu>
2021-12-13 23:36:30 +0300

    code fixed

--- src/main/java/ru/sbt/mipt/oop/commands/DummySensorCommander.java
+++ src/main/java/ru/sbt/mipt/oop/commands/DummySensorCommander.java
@@ -4,10 +4,6 @@ import java.util.logging.Logger;
 public class DummySensorCommander implements SensorCommander {
 	private static final Logger LOGGER = Logger.getLogger( DummySensorCommander.class.getName() );
 	
-	private DummySensorCommander() {
-		throw new IllegalStateException("Utility class");
-    }
-	
 	public DummySensorCommander() {}
 	
     public static void sendCommand(SensorCommand command) {
