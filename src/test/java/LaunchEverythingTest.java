43762a6b94cca07cae103f13841e079dacea8cd2
heavyrainik <sobolev.as@phystech.edu>
2021-12-13 19:14:24 +0300

    test

--- src/test/java/LaunchEverythingTest.java
+++ src/test/java/LaunchEverythingTest.java
@@ -1,5 +1,5 @@
 import com.coolcompany.smarthome.events.SensorEventsManager;
-import config.SmartHomeConfiguration;
+import SmartHomeConfiguration;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
