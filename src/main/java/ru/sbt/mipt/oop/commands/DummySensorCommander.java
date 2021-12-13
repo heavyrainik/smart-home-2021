080b6744da0521ec789f9aea2dede15ca9399206
heavyrainik <sobolev.as@phystech.edu>
2021-12-13 23:37:28 +0300

    code fixed

--- src/main/java/ru/sbt/mipt/oop/commands/DummySensorCommander.java
+++ src/main/java/ru/sbt/mipt/oop/commands/DummySensorCommander.java
@@ -1,12 +1,8 @@
 package ru.sbt.mipt.oop.commands;
-import java.util.logging.Logger;
 
 public class DummySensorCommander implements SensorCommander {
-	private static final Logger LOGGER = Logger.getLogger( DummySensorCommander.class.getName() );
-	
 	public DummySensorCommander() {}
 	
     public static void sendCommand(SensorCommand command) {
-        LOGGER.info(LOGGER.getName());
     }
 }
